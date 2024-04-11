9fb3e3dc9ae1a484b8748efe05a02e72c96ca637
tatiisft <114707011+tatiisft@users.noreply.github.com>
2024-04-11 20:34:34 -0300

    Criei um subtitulo e adicionei um estilo.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import './style.css'
 
 const Home: React.FC = () => {
   return (
     <>
       <h1>Home</h1>
+      <p className='titulo'>Olá mundo</p>
+      <div className="titulo">
+        <h1>EcoPoint</h1>
+      </div>
     </>
   )
 }
