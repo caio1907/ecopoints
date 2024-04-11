68406943fa037c0259d44e78deaa6d6b9673fdd1
tatiisft <114707011+tatiisft@users.noreply.github.com>
2024-04-11 19:55:34 -0300

    Commit teste

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './estilo.css'
 
 const Home: React.FC = () => {
   return (
     <>
-      <h1>Home</h1>
+      <div class='titulo'>
+        <h1>EcoPoint</h1>
+        <p>Commit teste</p>
+      </div>
     </>
   )
 }
